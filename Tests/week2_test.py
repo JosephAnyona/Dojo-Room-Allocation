e82460f6ee3fa238a5a8a4163e5ae31aa5da862b
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-16 17:49:41 +0300

    Update week2_test.py
    Added tests for create room and add person

--- Tests/week2_test.py
+++ Tests/week2_test.py
@@ -1,40 +1,49 @@
 import unittest
-from room import week2_class
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from System.room import Room
+
 
 class TestCreateRoom(unittest.TestCase):
-	def test_create_room_successfully(self):
-	    my_class_instance = week2_class()
 
-	    initial_room_count = len(my_class_instance.all_rooms)
-	    blue_office = my_class_instance.create_room("office", "Blue")	    
-	    self.assertTrue(blue_office)
-	    red_living = my_class_instance.create_room("living", "red")
-	    self.assertTrue(red_living)
+    def setUp(self):
+        self.class_instance = Room()
 
-	    offices = my_class_instance.offices
-	    new_room_count = len(my_class_instance.all_rooms)
-	    self.assertEqual(new_room_count - initial_room_count, 2)
+    def test_create_room_successfully(self):
+        initial_room_count = len(self.class_instance.all_rooms['offices'])
+        blue_office = self.class_instance.create_room("office", "Blue")
+        for key in self.class_instance.offices:
+            if key == "Blue_office":
+                self.assertIn("Blue_office", key)
+        offices = self.class_instance.offices
+        new_room_count = len(self.class_instance.all_rooms['offices'])
+        self.assertEqual(new_room_count - initial_room_count, 1)
 
+    def test_room_exists(self):
+        # self.assertEqual('hey green_living  exists, try another name',)
+        green_living = self.class_instance.create_room("living", "green")
+        self.assertEqual(
+            'hey green_living exists, try another name', green_living)
 
-	def test_add_person_successfully(self):
-	    my_class_instance = week2_class()
+    def test_add_person_successfully(self):
+        initial_people_count = len(self.class_instance.people)
+        # multiple adds checking fellow/staff and wants accomodation
+        joey_add = self.class_instance.add_person("joey", "FELLOW", "No")
+        jane_add = self.class_instance.add_person("jane", "STAFF", "Y")
+        viktor_add = self.class_instance.add_person("viktor", "STAFF", "No")
+        emily_add = self.class_instance.add_person("emily", "FELLOW", "Y")
+        self.assertTrue(joey_add)
+        self.assertTrue(jane_add)
+        self.assertTrue(viktor_add)
+        self.assertTrue(emily_add)
+        # people = class_instance.people
+        new_people_count = len(self.class_instance.people)
+        self.assertEqual(new_people_count - initial_people_count, 4)
 
-	    initial_people_count = len(my_class_instance.people)
-
-	    joey_add = my_class_instance.add_person("joey", "FELLOW", "No")
-	    jane_add = my_class_instance.add_person("jane", "STAFF", "Y")
-	    viktor_add = my_class_instance.add_person("viktor", "STAFF", "No")
-	    emily_add = my_class_instance.add_person("emily", "FELLOW", "Y")
-
-	    self.assertTrue(joey_add)
-
-	    # people = my_class_instance.people
-
-	    new_people_count = len(my_class_instance.people)
-
-	    self.assertEqual(new_people_count - initial_people_count, 4)
+    def test_room_allocation(self):
+        derek_add_allocate = self.class_instance.add_person(
+            "derek", "FELLOW", "Y")
 
 if __name__ == "__main__":
-	unittest.main()
-
-
+    unittest.main()
