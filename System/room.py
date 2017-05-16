979f9e34ddcf0c6fea5a19d564a6c629679ce16f
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-16 17:48:54 +0300

    Update room.py
    Create room and add person changed and working.

--- System/room.py
+++ System/room.py
@@ -1,79 +1,100 @@
 """Usage:
   room.py create_room (office|living) <room_name>...
   room.py add_person <person_name> <FELLOW|STAFF> [wants_accommodation]
+  room.py print_room <room_name>
 
 """
 
-class week2_class(object):
-  def __init__ (self):
-    #rooms
-    self.all_rooms = []
-    self.offices=[]
-    self.livingSpaces = []
 
-    #people
-    self.people = []
-    self.fellows = []
-    self.staff = []
+class Room(object):
 
-    
-  def create_room(self, room_type, room_name):
-    # print(names)
-    if room_type == 'office':
-      names = [room_name]
-      for name in names:
-        office_name = (name+"_office")
-        self.offices.append(office_name)
+    def __init__(self):
+        # rooms
+        # 2 keys, offices living sapces
+        self.all_rooms = {'offices': [], 'livingSpaces': ['green_living']}
+        self.offices = {'green_living': []}
+        self.livingSpaces = {}
+        self.vacant_office = []
+        self.vacant_living = []
+        # people
+        self.people = []
+        self.fellows = []
+        self.staff = []
+        # allocated
+        self.allocated = []
+        self.unallocated = {'Fellows': [], 'Staff': []}
 
-        self.all_rooms.append(office_name)
-        # print(room_name, room_type)
-        print(self.offices)
-        return('An office called ' + name + ' has been successfully created!')
+    def create_room(self, room_type, room_name):
+        # print(names)
+        space_name = (room_name+"_"+room_type)
+       # check if the space exists
+        for key in self.offices:
+            if space_name == key:
+                return("hey "+space_name+" exists, try another name")
+            else:
+                pass
 
-    elif room_type == 'living':
-      names = [room_name]
-      for name in names:
-        office_name = (name+"_living")
-        self.livingSpaces.append(office_name)
+        if room_type == 'office':
+            # create new key with office name and append to offices
+            self.offices[space_name] = None
+            # append to dictionary using key offices
+            self.all_rooms['offices'].append(space_name)
+            print(self.all_rooms)
+            print(self.offices)
+            print('An '+room_type + ' space called ' + space_name +
+                  ' has been successfully created!')
+            # print(room_name, room_type)
+            return self.all_rooms['offices']
 
-        self.all_rooms.append(office_name)
+        elif room_type == 'living':
+            # create new key with office name and append to offices
+            self.offices[space_name] = None
+            # append to all_rooms dictionary using key offices
+            self.all_rooms['livingSpaces'].append(space_name)
+            print(self.all_rooms)
+            print(self.offices)
+            print('A '+room_type + ' space called ' + space_name +
+                  ' has been successfully created!')
 
-        print(self.all_rooms)
-        print('A living space called ' + name + ' has been successfully created!')
+        return self.all_rooms['livingSpaces']
 
-    return self.all_rooms
+    def room_allocation(self, person_name, wants_accommodation):
+        # getting from add person
+        person_name = self.add_person.person_name
+        wants_space = "Yes" if self.add_person.wants_accommodation is "Y" else "No"
 
+    def add_person(self, person_name, person_type, wants_accommodation):
+        # staff - no accommodation
+        wants_space = "Yes" if wants_accommodation is "Y" else "No"
 
+        if wants_space == "No":
+            if person_type == "FELLOW":
+                self.people.append(person_name)  # add to people
+                self.fellows.append(person_name)  # add to fellows
+                print(self.people)
+                print('Fellow ' + person_name +
+                      ' has been successfully added with no accommodation')
 
-  def add_person(self, person_name, person_type, wants_accommodation):
-    #staff - no accommodation
-    wants_space = "Yes" if wants_accommodation is "Y" else "No"
+            elif person_type == "STAFF":
+                self.people.append(person_name)  # add to people
+                self.staff.append(person_name)  # add to staff
+                print(self.people)
+                print('Staff ' + person_name +
+                      ' has been successfully added')
 
-    if wants_space == "No":
-      if person_type == "FELLOW":
-        self.people.append(person_name) #add to people
-        self.fellows.append(person_name)#add to fellows
-        print(self.people)
-        print('Fellow ' + person_name + ' has been successfully added with no accommodation')
+        if wants_space == "Yes":
+            if person_type == "FELLOW":
+                self.people.append(person_name)  # add to people
+                self.fellows.append(person_name)  # add to fellows
+                print(self.people)
+                print('Fellow ' + person_name +
+                      ' has been successfully added with accommodation')
 
-      elif person_type == "STAFF":
-        self.people.append(person_name) #add to people
-        self.staff.append(person_name) #add to staff
-        print(self.people)
-        print('Staff ' + person_name + ' has been successfully added')
+            elif person_type == "STAFF":
+                self.people.append(person_name)  # add to people
+                self.staff.append(person_name)  # add to staff
+                print(self.people)
+                print('Staff can not be given accommodation')
+                print('Staff ' + person_name + ' has been successfully added')
 
-    if wants_space == "Yes":
-        if person_type == "FELLOW":
-            self.people.append(person_name) #add to people
-            self.fellows.append(person_name)#add to fellows
-            print(self.people)
-            print('Fellow ' + person_name + ' has been successfully added with accommodation')
-
-        elif person_type == "STAFF":
-          self.people.append(person_name) #add to people
-          self.staff.append(person_name) #add to staff
-          print(self.people)
-          print('Staff can not be given accommodation')
-          print('Staff ' + person_name + ' has been successfully added')
-
-    return self.people
+        return self.people
