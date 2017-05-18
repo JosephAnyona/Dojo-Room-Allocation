85a57ed05d60bb60d20ff9dc2568cf7a1ffa3c66
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-18 15:04:17 +0300

    Class person
    contains fellow and staff

--- System/person.py
+++ System/person.py
@@ -1,24 +1,14 @@
-class Person(object):
-   # Creates Person object where the class Office and Livingspace inherit from
-
-    def __init__(self, name):
-        self.name = name
-
-
-class Fellow(Person):
-    person_type = "FELLOW"
-
-    def __init__(self, name):
-        Person.__init__(self, name)
-        
-class Staff(Person):
-
-    person_type = "STAFF"
-
-
-
+class Person(object):
+   # Creates Person object where the class Office and Livingspace inherit from
     def __init__(self, name):
+        self.name = name
 
+class Fellow(Person):
+    person_type = "FELLOW"
+    def __init__(self, name):
         Person.__init__(self, name)
 
-
+class Staff(Person):
+    person_type = "STAFF"
+    def __init__(self, name):
+        Person.__init__(self, name)
