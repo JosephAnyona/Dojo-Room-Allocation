e676bfdc7e48a36989f57d0ad5dec60be75912b1
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-18 14:48:54 +0300

    Update week2_test.py
    additional functionalities

--- Tests/week2_test.py
+++ Tests/week2_test.py
@@ -1,14 +1,14 @@
 import unittest
 import sys
 from os import path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from System.room import Room
+sys.path.append('../System')
+from dojo import Dojo
 
 
 class TestCreateRoom(unittest.TestCase):
 
     def setUp(self):
-        self.class_instance = Room()
+        self.class_instance = Dojo()
 
     def test_create_room_successfully(self):
         initial_room_count = len(self.class_instance.all_rooms['offices'])
@@ -20,14 +20,19 @@ class TestCreateRoom(unittest.TestCase):
         new_room_count = len(self.class_instance.all_rooms['offices'])
         self.assertEqual(new_room_count - initial_room_count, 1)
 
-    # def test_room_exists(self):
-    #     green_living = self.class_instance.create_room(
-    #         "living", "green")  # no hardcode
-    #     self.assertEqual(
-    #         'hey green_living exists, try another name', green_living)
-    #     chrome_office = self.class_instance.create_room("office", "chrome")
-    #     self.assertEqual(
-    #         'hey chrome_office exists, try another name', chrome_office)
+    def test_room_exists(self):
+        green_living1 = self.class_instance.create_room(
+            "living", "green")  # no hardcode
+        green_living2 = self.class_instance.create_room(
+            "living", "green")
+
+        self.assertEqual(
+            'hey green_living exists, try another name', green_living2)
+
+        chrome_office1 = self.class_instance.create_room("office", "chrome")
+        chrome_office2 = self.class_instance.create_room("office", "chrome")
+        self.assertEqual(
+            'hey chrome_office exists, try another name', chrome_office2)
 
     def test_room_invalid__room_type(self):
         invalid_room_type = self.class_instance.create_room("invalid", "green")
@@ -44,17 +49,17 @@ class TestCreateRoom(unittest.TestCase):
         add_room2
         # multiple adds checking fellow/staff and wants accomodation
         initial_people_count = len(self.class_instance.people['FELLOW'])
-        joey_add = self.class_instance.add_person("joey", "FELLOW", "No")
-        emily_add = self.class_instance.add_person("emily", "FELLOW", "Y")
-        joey_add
-        emily_add
-        self.assertIn(self.class_instance.fellows, [
-                      joey_add])  # assert in/equal
-        self.assertIn(str(self.class_instance.fellows), emily_add)
+        self.class_instance.add_person("joey", "FELLOW", "No")
+        self.class_instance.add_person("emily", "FELLOW", "Y")
+
+        self.assertEqual("joey" in self.class_instance.fellows, True)
+        self.assertEqual("emily" in self.class_instance.fellows, True)
+
         new_people_count = len(self.class_instance.people['FELLOW'])
         self.assertEqual(new_people_count - initial_people_count, 2)
         # multiple adds checking fellow/staff and wants accomodation
         initial_people_count = len(self.class_instance.people['STAFF'])
+
         jane_add = self.class_instance.add_person("jane", "STAFF", "Y")
         viktor_add = self.class_instance.add_person("viktor", "STAFF", "No")
         self.assertIn('jane', jane_add)
@@ -80,6 +85,7 @@ class TestCreateRoom(unittest.TestCase):
         add_room
         add_person
         self.assertIn("emily", self.class_instance.livingSpaces["mara_living"])
+        print_room = self.class_instance.print_room
 
     def test_vacant_space(self):
         add_room = self.class_instance.create_room("living", "mara")
