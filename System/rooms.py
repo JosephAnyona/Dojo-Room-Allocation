a487e0018020bddf73817dc1d4d1d119f3f115b7
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-18 14:52:22 +0300

    Update rooms.py
    Class Room contains office, livingSpace

--- System/rooms.py
+++ System/rooms.py
@@ -1,20 +1,17 @@
-class Room(object):
+class Room(object):
+
     # Creates Room object where the class Office and Livingspace inherit from
     def __init__(self, name):
         self.name = name
-
 
 class Office(Room):
     capacity = 6
 
     def __init__(self, name):
-        Room.__init__(self, name)
-
-
+        Room.__init__(self, name)
 
 class LivingSpace(Room):
     capacity = 4    
 
     def __init__(self, name):
-        Room.__init__(self, name)
-
+        Room.__init__(self, name)
