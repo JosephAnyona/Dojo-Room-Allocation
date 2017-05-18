f3b05cdbb39e5b57f852e08a5c9da025d7bbe9d5
Joseph Anyona <anyonajoseph@gmail.com>
2017-05-18 14:51:10 +0300

    Add files via upload
    additional classes and functionalities

---
+++ System/person.py
@@ -0,0 +1,24 @@
+class Person(object):
+   # Creates Person object where the class Office and Livingspace inherit from
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Fellow(Person):
+    person_type = "FELLOW"
+
+    def __init__(self, name):
+        Person.__init__(self, name)
+        
+class Staff(Person):
+
+    person_type = "STAFF"
+
+
+
+    def __init__(self, name):
+
+        Person.__init__(self, name)
+
+
